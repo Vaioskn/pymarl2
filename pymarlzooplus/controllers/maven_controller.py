b31f94aa1f1942330340eaeed2631f1a6463c43f
Apostolos Varelas <87095756+ApostolosVarelas@users.noreply.github.com>
2025-07-04 16:59:07 +0300

    commit

---
+++ pymarlzooplus/controllers/maven_controller.py
@@ -0,0 +1,51 @@
+from .basic_controller import BasicMAC
+import torch as th
+
+
+# This multi-agent controller shares parameters between agents
+class Maven(BasicMAC):
+    def __init__(self, scheme, groups, args):
+        super().__init__(scheme, groups, args)
+
+    def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
+        extra_returns = {}
+        # Only select actions for the selected batch elements in bs
+        avail_actions = ep_batch["avail_actions"][:, t_ep]
+        agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
+        chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
+        return chosen_actions, extra_returns
+
+    def forward(self, ep_batch, t, test_mode=False):
+        agent_inputs = self._build_inputs(ep_batch, t)
+        avail_actions = ep_batch["avail_actions"][:, t]
+        noise_vector = ep_batch["noise"][:, 0]
+        agent_outs, self.hidden_states = self.agent(agent_inputs, self.hidden_states, noise_vector)
+
+        if self.agent_output_type == "pi_logits":
+
+            if getattr(self.args, "mask_before_softmax", True):
+                # Make the logits for unavailable actions very negative to minimise their affect on the softmax
+                reshaped_avail_actions = avail_actions.reshape(ep_batch.batch_size * self.n_agents, -1)
+                agent_outs[reshaped_avail_actions == 0] = -1e10
+
+            agent_outs = th.nn.functional.softmax(agent_outs, dim=-1)
+            if not test_mode:
+                # Epsilon floor
+                epsilon_action_num = agent_outs.size(-1)
+                if getattr(self.args, "mask_before_softmax", True):
+                    # With probability epsilon, we will pick an available action uniformly
+                    epsilon_action_num = reshaped_avail_actions.sum(dim=1, keepdim=True).float()
+
+                agent_outs = ((1 - self.action_selector.epsilon) * agent_outs
+                               + th.ones_like(agent_outs) * self.action_selector.epsilon/epsilon_action_num)
+
+                if getattr(self.args, "mask_before_softmax", True):
+                    # Zero out the unavailable actions
+                    agent_outs[reshaped_avail_actions == 0] = 0.0
+
+        return agent_outs.view(ep_batch.batch_size, self.n_agents, -1)
+    
+    def init_hidden(self, batch_size):
+        self.hidden_states = self.agent.init_hidden().unsqueeze(0).expand(batch_size, self.n_agents, -1)
+
+    
---
+++ pymarlzooplus/modules/agents/__init__.py
@@ -0,0 +1,18 @@
+from .rnn_agent import RNNAgent
+from .rnn_ns_agent import RNNNSAgent
+from .mlp_mat_agent import MLPMATAgent
+from .rnn_agent_happo import RNNAgentHAPPO
+from .rnn_agent_emc import RNNAgentEMC
+from .rnn_agent_cds import RNNAgentCDS
+from .rnn_maven_agent import RNNAgentMaven
+
+REGISTRY = {"rnn": RNNAgent,
+            "rnn_ns": RNNNSAgent,
+            "mlp_mat": MLPMATAgent,
+            "rnn_happo": RNNAgentHAPPO,
+            "rnn_emc": RNNAgentEMC,
+            "rnn_cds": RNNAgentCDS,
+            "rnn_maven": RNNAgentMaven
+            }
+
+
